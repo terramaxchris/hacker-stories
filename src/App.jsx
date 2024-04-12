7af8bf5e409d930594a6b2d8a3071c120a0a81f8
Chris Sanders <chris.sanders@terramaxag.com>
2024-04-12 13:24:06 -0500

    added api to pull stories from some site

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,160 @@
 /* eslint-disable react/prop-types */
 import * as React from 'react';
 
-const welcome = {
-  title: "React",
-  greeting: "Hey",
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
+
+// const initialStories = [
+//   {
+//     title: 'React',
+//     url: 'https//reactjs.org',
+//     author: 'Jordan Walke',
+//     num_comments: 3,
+//     points: 4,
+//     objectID: 0,
+//   },
+//   {
+//     title: 'Redux',
+//     url: 'https//redux.js.org',
+//     author: 'Dan Abramov, Andrew Clark',
+//     num_comments: 2,
+//     points: 5,
+//     objectID: 1,
+//   }
+// ];
+
+// const getAsyncStories = () => 
+//   new Promise((resolve) => 
+//     setTimeout(
+//       () => resolve({data: {stories: initialStories } }),
+//       2000
+//     )
+//   );
+
+  const storiesReducer = (state, action) => {
+    switch (action.type) {
+      case 'STORIES_FETCH_INIT':
+        return {
+          ...state,
+          isLoading: true,
+          isError: false,
+        };
+      case 'STORIES_FETCH_SUCCESS':
+        return {
+          ...state,
+          isLoading: false,
+          isError: false,
+          data: action.payload,
+        };
+      case 'STORIES_FETCH_FAILURE':
+        return {
+          ...state,
+          isLoading: false,
+          isError: true,
+        };
+      case 'REMOVE_STORY':
+        console.log("whut");
+        return {
+          ...state,
+          data: state.data.filter(
+            (story) => action.payload.objectID !== story.objectID
+          ),
+        };
+      default:
+        throw new Error();
+    }
+  };
+
+const useStorageState = (key, initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  return [value, setValue];
 };
+
+function App() {
+  // eslint-disable-next-line no-unused-vars
+  const [searchTerm, setSearchTerm] = useStorageState('storage', 'React');
+
+  const [stories, dispatchStories] = React.useReducer(
+    storiesReducer, 
+    { data: [], isLoading: false, isError: false }
+  );
+
+  React.useEffect(() => {
+    dispatchStories({ type: 'STORIES_FETCH_INIT' });
+
+    fetch(`${API_ENDPOINT}react`)
+      .then((response) => response.json())
+      .then((result) => {dispatchStories({
+          type: 'STORIES_FETCH_SUCCESS',
+          payload: result.hits,
+        });
+      })
+      .catch(() => 
+        dispatchStories({
+          type: 'STORIES_FETCH_FAILURE' })
+      );
+
+    // getAsyncStories()
+    //   .then((result) => {
+    //     dispatchStories({
+    //       type: 'STORIES_FETCH_SUCCESS',
+    //       payload: result.data.stories,
+    //     });
+    //   })
+    //   .catch(() => 
+    //     dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
+    //   );
+  }, []);
+
+  const handleRemoveStory = (item) => {
+    dispatchStories({
+      type: "REMOVE_STORY",
+      payload: item,
+    });
+    console.log(stories)
+  };
+
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+
+    localStorage.setItem('search', event.target.value);
+  }
+  const searchedStories = stories.data.filter((story) => story.title.toLowerCase().includes(searchTerm.toLowerCase()));
+
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      <InputWithLabel
+        id="search"
+        value={searchTerm}
+        isFocused
+        onInputChange={handleSearch}>
+          <strong>Search: </strong>
+        </InputWithLabel>
+      
+      <hr />
+
+      {stories.isError && <p>Something went wrong ...</p>}
+
+      {stories.isLoading ? (
+        <p>Loading...</p>
+      ) : ( 
+      <List 
+        list={searchedStories} 
+        onRemoveItem={handleRemoveStory}
+      />
+      )}
+    </div>
+  );
+}
 
 const InputWithLabel = ({ id, value, onInputChange, isFocused, children }) => {
   
@@ -29,32 +179,26 @@ const InputWithLabel = ({ id, value, onInputChange, isFocused, children }) => {
       id={id} 
       type="text" 
       value={value} 
-      autoFocus={isFocused} 
       onChange={onInputChange} 
       />
   </>
 )}
 
-const List = ({ list, deleteItem }) => {
-  console.log("List renders");
-
-    return (
+const List = ({ list, onRemoveItem }) => {
+  return (
     <ul>
-        {list
-          .map((item) => (
-            <Item key={item.objectID} item={item} deleteItem={deleteItem} />
-          )
-      )}
+        {list.map((item) => (
+          <Item 
+            key={item.objectID} 
+            item={item} 
+            onRemoveItem={onRemoveItem} 
+          />
+        ))}
       </ul>
     );
-  }
+  };
 
-const Item = ({ item, deleteItem }) => {
-const handleDeleteItem = () => {
-  deleteItem(item);
-  console.log(item);
-}
-
+const Item = ({ item, onRemoveItem }) => {
   return (
     <li>
       <span>
@@ -63,117 +207,9 @@ const handleDeleteItem = () => {
       <span>{item.author}, </span>
       <span>{item.num_comments} comments, </span>
       <span>{item.points} points</span>
-      <button onClick={handleDeleteItem}>Delete Book</button>
+      <button type='button' onClick={() => onRemoveItem(item)}>Delete Book</button>
     </li>
   )
-}
-
-const useStorageState = (key, initialState) => {
-  const [value, setValue] = React.useState(
-    localStorage.getItem(key) || initialState
-  );
-
-  React.useEffect(() => {
-    localStorage.setItem(key, value);
-  }, [value, key]);
-
-  return [value, setValue];
-}
-
-const initialStories = [
-  {
-    title: 'React',
-    url: 'https//reactjs.org',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https//redux.js.org',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  }
-];
-
-const getAsyncStories = () => 
-  //Promise.resolve({ data: { stories: initialStories } });
-  new Promise((resolve) => 
-    setTimeout(
-      () => resolve({data: {stories: initialStories } }),
-      2000
-    )
-  );
-
-function App() {
-  console.log("App renders");
-
-  // eslint-disable-next-line no-unused-vars
-  const [searchTerm, setSearchTerm] = useStorageState('storage', 'React');
-  const [stories, setStories] = React.useState([]);
-  const [isLoading, setIsLoading] = React.useState(false);
-  const [isError, setIsError] = React.useState(false);
-
-  React.useEffect(() => {
-    getAsyncStories().then(result => {
-      setStories(result.data.stories);
-    });
-  }, []);
-
-  React.useEffect(() => {
-    setIsLoading(true);
-    getAsyncStories().then((result) => {
-      setStories(result.data.stories)
-      setIsLoading(false);
-    })
-    .catch(() => setIsError(true));
-  }, []);
-
-  const handleSearch = (event) => {
-    setSearchTerm(event.target.value);
-
-    localStorage.setItem('search', event.target.value);
-  }
-
-  const deleteStory = (item) => {
-    //changeStories(stories.filter((story) => story.objectID !== ))
-    console.log(item);
-    const newStories = stories.filter((story) => item.objectID !== story.objectID);
-    setStories(newStories);
-  }
-
-  const filteredStories = stories.filter((story) => story.title.toLowerCase().includes(searchTerm.toLowerCase()));
-
-  // if (isLoading) {
-  //   return <p>Loading...</p>;
-  // }
-
-  return (
-    <div>
-      <h1>{welcome.greeting}, {welcome.title}</h1>
-
-      <InputWithLabel
-        id="search"
-        value={searchTerm}
-        isFocused={false}
-        onInputChange={handleSearch}>
-          <strong>Search: </strong>
-        </InputWithLabel>
-      
-      <hr />
-
-      {isError && <p>Something went wrong ...</p>}
-
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : ( 
-      <List list={filteredStories} deleteItem={deleteStory}/>
-      )}
-    </div>
-  );
-}
+};
 
 export default App;
